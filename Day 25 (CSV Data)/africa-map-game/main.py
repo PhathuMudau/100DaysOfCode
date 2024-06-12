248a935b8319a84b5aa5f53b3cfa77bcd31efc23
Phathu <phathu.mudau@yahoo.com>
2024-06-12 22:26:02 +0200

    African Countries Game

---
+++ Day 25 (CSV Data)/africa-map-game/main.py
@@ -0,0 +1,51 @@
+import turtle
+import pandas as pd
+
+
+screen = turtle.Screen()
+screen.setup(width=800, height=700)
+screen.title("African Countries Game")
+image = "africa-blank-colorful.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pd.read_csv("african_countries.csv")
+
+total = len(data.country)
+
+country_list = data.country.to_list()
+guessed_countries = []
+
+while len(guessed_countries) < len(data.country):
+
+    answer_country = screen.textinput(title=f"{len(guessed_countries)}/{total} Countries Correct",
+                                      prompt="Guess a country? Type exit when finished.")
+
+    if answer_country == "Exit":
+        missing_countries = []
+        for country in country_list:
+            if country not in guessed_countries:
+                missing_countries.append(country)
+        new_data = pd.DataFrame(missing_countries)
+        new_data.to_csv("Countries_to_Learn.csv")
+        break
+
+    if answer_country is None:
+        pass
+    elif answer_country.title() in country_list:
+        answer = answer_country.title()
+        if answer_country not in guessed_countries:
+            guessed_countries.append(answer)
+        t = turtle.Turtle()
+        t.penup()
+        t.hideturtle()
+        country_data = data[data.country == answer_country]
+        t.goto(int(country_data.x.iloc[0]), int(country_data.y.iloc[0]))
+        t.write(answer)
+
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+#
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
